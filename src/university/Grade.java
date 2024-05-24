7eaf0d79ab9a7d961ca9b88adeec778bf62a46a2
Jonathan Alvindo Fernandi <jonathanafernandi@gmail.com>
2024-05-24 21:10:38 +0700

    add grade and subject

---
+++ src/university/Grade.java
@@ -0,0 +1,26 @@
+package university;
+
+public class Grade {
+	private Student student;
+	private Subject subject;
+	private int score;
+
+	public Grade(Student student, Subject subject, int score) {
+		this.student = student;
+		this.subject = subject;
+		this.score = score;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public int getScore() {
+		return score;
+	}
+	
+}
